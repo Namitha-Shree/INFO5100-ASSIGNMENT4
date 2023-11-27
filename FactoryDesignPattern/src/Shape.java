0735c06d3ebf8bb174855fdf8089891f64500444
Namitha Shree Thalanki Narayanamurthy <thalankinarayanamu.n@northeastern.edu>
2023-11-27 17:41:40 -0500

    .

---
+++ FactoryDesignPattern/src/Shape.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+/**
+ * Shape class defines the interface for shapes.
+ *
+ * @author Namitha
+ * @version 1.0
+ */
+public interface Shape {
+    void draw();
+}
